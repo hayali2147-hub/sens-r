07d8051b3d4cb0234032770c79d5986f7dc2160f
ARMAHACK <hayali.2147@gmail.com>
2026-08-03 23:52:25 +0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,41 @@
+// =====================================
+// AirDraw Enterprise Server
+// =====================================
 
+
+const express = require("express");
+
+const app = express();
+
+
+app.use(
+express.json()
+);
+
+
+
+app.get("/",(req,res)=>{
+
+
+res.json({
+
+status:"AirDraw AI Online",
+
+version:"Enterprise"
+
+});
+
+
+});
+
+
+
+app.listen(3000,()=>{
+
+
+console.log(
+"Server çalışıyor"
+);
+
+
+});
