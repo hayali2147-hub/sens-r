ca602a4236222bff35a6f492293b04a65ecc2b15
ARMAHACK <hayali.2147@gmail.com>
2026-08-03 23:56:50 +0300

    Add files via upload

--- security/auth.js
+++ security/auth.js
@@ -1,1 +1,39 @@
+// =====================================
+// Authentication
+// =====================================
 
+
+
+function createUser(name){
+
+
+return {
+
+
+id:
+Date.now(),
+
+
+name:name,
+
+
+created:
+new Date()
+
+
+};
+
+
+}
+
+
+
+
+function checkUser(user){
+
+
+return user &&
+user.id;
+
+
+}
