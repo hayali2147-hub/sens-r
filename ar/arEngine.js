9842b518d1d3b1d793a76a9c1fdff231c47c50da
ARMAHACK <hayali.2147@gmail.com>
2026-08-03 23:51:22 +0300

    Add files via upload

--- ar/arEngine.js
+++ ar/arEngine.js
@@ -1,1 +1,40 @@
+// =====================================
+// AR Engine
+// =====================================
 
+
+let arEnabled=false;
+
+
+
+function startAR(){
+
+
+arEnabled=true;
+
+
+}
+
+
+
+function stopAR(){
+
+
+arEnabled=false;
+
+
+}
+
+
+
+function renderAR(){
+
+
+if(!arEnabled)
+return;
+
+
+// AR efektleri buraya gelecek
+
+
+}
