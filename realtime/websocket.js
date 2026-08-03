1f51d7d673879be9a8389eee8ad9a7a97a0c9e9c
ARMAHACK <hayali.2147@gmail.com>
2026-08-03 23:55:52 +0300

    Add files via upload

--- realtime/websocket.js
+++ realtime/websocket.js
@@ -1,1 +1,34 @@
+// =====================================
+// Real Time Collaboration
+// =====================================
 
+
+const clients=[];
+
+
+
+function addClient(socket){
+
+
+clients.push(socket);
+
+
+}
+
+
+
+function broadcast(data){
+
+
+clients.forEach(client=>{
+
+
+client.send(
+JSON.stringify(data)
+);
+
+
+});
+
+
+}
